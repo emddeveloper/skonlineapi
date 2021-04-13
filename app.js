e8f8443996e2a6b1e6c797176eccaca6c03050dd
emddeveloper <emd.developer@gmail.com>
2021-04-13 15:36:59 +0530

    ss

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.get("/records", function (req, res) {
   res.send([{ id: "1", timestamp: 1618228109367, cash: 5000, jio: 5000, bank: 5000, credit: "", profit: 78, total: 35 }])
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT
 app.listen(port)
