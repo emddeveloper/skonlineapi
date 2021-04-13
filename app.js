5aae2a2617b8e42822c656dba19e44ec65d8c921
emddeveloper <emd.developer@gmail.com>
2021-04-13 12:02:40 +0530

    data done records

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ var app = express()
 app.get("/", function (req, res) {
   res.send("Hello world!")
 })
+app.get("/records", function (req, res) {
+  res.send([{ id: "1", timestamp: 1618228109367, cash: 5000, jio: 5000, bank: 5000, credit: "", profit: 78, total: 35 }])
+})
 const port = process.env.PORT || 4000
 app.listen(port)
