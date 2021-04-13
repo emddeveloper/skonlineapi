48eb6018463d442f0838b4088d2d6e9f45b09b62
emddeveloper <emd.developer@gmail.com>
2021-04-13 12:34:01 +0530

    mysql

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var express = require("express")
 var app = express()
 var cors = require("cors")
 
+const mysql = require("mysql")
+const bodyParser = require("body-parser")
+
 app.use(cors())
 
+const connection = mysql.createConnection({
+  host: "sql328.main-hosting.eu",
+  user: "u769955481_mybalance",
+  password: "mybalance@123",
+  database: "u769955481_mybalance"
+})
+connection.connect(function (error) {
+  if (!!error) console.log(error)
+  else console.log("Database Connected!")
+})
 app.get("/records/:id", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" + req })
 })
@@ -14,5 +27,6 @@ app.get("/", function (req, res) {
 app.get("/records", function (req, res) {
   res.send([{ id: "1", timestamp: 1618228109367, cash: 5000, jio: 5000, bank: 5000, credit: "", profit: 78, total: 35 }])
 })
+
 const port = process.env.PORT || 4000
 app.listen(port)
